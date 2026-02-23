e4f52f755f6cd4d9e66fa1b26b0edcfe7fe16f64
Danielito1996 <daniel.imbert96@hotmail.com>
2026-02-23 12:29:19 -0600

    product case use implements

--- src/feautures/product/di/product.container.ts
+++ src/feautures/product/di/product.container.ts
@@ -1,6 +1,9 @@
 import {infrastructureContainer} from "../../../infrastructure/di/infrastructure.container";
 import ProductNetRepository from "../data/repository/product.net.repository";
 import {ProductOfflineFirstRepository} from "../data/repository/product.offline-first.repository";
+import {GetAllProductCaseUse} from "../domain/caseuse/GetAllProductCaseUse";
+import {GetProductByIdCaseUse} from "../domain/caseuse/GetProductByIdCaseUse";
+import {SaveProductCaseUse} from "../domain/caseuse/SaveProductCaseUse";
 
 // Database instance
 let database = infrastructureContainer.appwrite.databases
@@ -8,3 +11,10 @@ let database = infrastructureContainer.appwrite.databases
 // Data
 const productNetRepository = new ProductNetRepository(database)
 const productOfflineFirstRepository = new ProductOfflineFirstRepository(productNetRepository)
+
+// Domain
+const getAllProductsCaseUse = new GetAllProductCaseUse(productOfflineFirstRepository)
+const deletedProductCaseUse = new GetAllProductCaseUse(productOfflineFirstRepository)
+const getProductByIdCaseUse = new GetProductByIdCaseUse(productOfflineFirstRepository)
+const modifyProductCaseUse = new GetProductByIdCaseUse(productOfflineFirstRepository)
+const saveProductCaseUse = new SaveProductCaseUse(productOfflineFirstRepository)
